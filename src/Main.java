eb246c7e140083426115300c9cd53e7ff2519d64
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-19 16:05:44 +0500

    added implementation for Linked list and added some code in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final MyList<Integer> ls = new MyLinkedList<>();
+
+    private static void testAdd(){
+        System.out.println("To add element index and item, -1 otherwise:");
+        int index = scanner.nextInt();
+        if(index != -1){
+            int item = scanner.nextInt();
+            ls.add(index, item);
+        }
+    }
+
+    private static void testPrint(){
+        System.out.print("Ls elements: ");
+        var a = ls.toArray();
+        for(var i : a)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+
+    private static void test(){
+        while(true) {
+            testAdd();
+            testPrint();
+        }
+    }
+
+    public static void main(String[] args) {
+        test();
     }
 }
