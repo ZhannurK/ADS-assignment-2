8bd1b4bdcce827907e1db2644119b9502b4e0143
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-21 19:34:45 +0500

    finished implementation for all method in MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -103,7 +103,16 @@ public class MyArrayList<T> implements MyList<T> {
 
     @Override
     public void sort() {
-
+        Object temp = 0;
+        boolean swapped = true;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = false;
+            }
+        }
     }
 
     @Override
