364160149dc8049218cabb79ab6832545968aeaf
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-23 21:13:35 +0500

    Added test method in main

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,73 @@
 import java.util.Scanner;
 
 public class Main {
-
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final MyList<Integer> ls = new MyLinkedList<>();
-
-    private static void testAdd(){
-        System.out.println("To add element index and item, -1 otherwise:");
-        int index = scanner.nextInt();
-        if(index != -1){
-            int item = scanner.nextInt();
-            ls.add(index, item);
+    public static <T> void printList(MyList<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
         }
-    }
-
-    private static void testPrint(){
-        System.out.print("Ls elements: ");
-        var a = ls.toArray();
-        for(var i : a)
-            System.out.print(i + " ");
         System.out.println();
     }
 
-    private static void test(){
-        while(true) {
-            testAdd();
-            testPrint();
-        }
+    public static void main(String[] args) {
+        testArrayList();
+        testLinkedList();
     }
 
-    public static void main(String[] args) {
-        test();
+    private static void testArrayList() {
+        MyList<Integer> arrayList = new MyLinkedList<>();
+
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        System.out.print("ArrayList: ");
+        printList(arrayList);
+
+        arrayList.add(1, 5);
+        System.out.print("ArrayList after adding 5 at index 1: ");
+        printList(arrayList);
+
+        arrayList.set(2, 10);
+        System.out.print("ArrayList after setting 10 at index 2: ");
+        printList(arrayList);
+
+        arrayList.remove(0);
+        System.out.print("ArrayList after removing element at index 0: ");
+        printList(arrayList);
+
+        System.out.println("Index of 10: " + arrayList.indexOf(10));
+        System.out.println("Does 5 exist: " + arrayList.exists(5));
+
+        arrayList.clear();
+        System.out.print("ArrayList after clear: ");
+        printList(arrayList);
+    }
+
+    private static void testLinkedList() {
+        MyList<String> linkedList = new MyLinkedList<>();
+
+        linkedList.add("Apple");
+        linkedList.add("Banana");
+        linkedList.add("Cherry");
+        System.out.print("LinkedList: ");
+        printList(linkedList);
+
+        linkedList.add(1, "Mango");
+        System.out.print("LinkedList after adding Mango at index 1: ");
+        printList(linkedList);
+
+        linkedList.set(2, "Grapes");
+        System.out.print("LinkedList after setting Grapes at index 2: ");
+        printList(linkedList);
+
+        linkedList.remove(0);
+        System.out.print("LinkedList after removing element at index 0: ");
+        printList(linkedList);
+
+        System.out.println("Index of Mango: " + linkedList.indexOf("Mango"));
+        System.out.println("Does Banana exist: " + linkedList.exists("Banana"));
+
+        linkedList.clear();
+        System.out.print("LinkedList after clear: ");
+        printList(linkedList);
     }
 }
