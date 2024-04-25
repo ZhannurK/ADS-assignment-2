b69e38e90984c33a96ce6e1fa4687035e871c13c
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-25 20:58:11 +0500

    wrote implementation for MyQueue

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,3 +1,57 @@
-public class MyQueue {
+public class MyQueue<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
 
+    public MyQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    private class Node<T> {
+        private T data;
+        private Node<T> next;
+        public Node(T data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void enqueue(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            return null;
+        }
+        T data = head.data;
+        head = head.next;
+        return data;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return head.data;
+    }
+
+    public int size() {
+        return size;
+    }
 }
