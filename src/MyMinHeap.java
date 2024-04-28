feff45fa249bab2b803c79f54a30bf1e0fd277c6
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-29 00:01:33 +0500

    wrote and finished implementation for MyMinHeap

--- src/MyMinHeap.java
+++ src/MyMinHeap.java
@@ -1,3 +1,82 @@
-public class MyMinHeap {
+public class MyMinHeap<T> {
+    private final MyArrayList<T> heap;
+    private final int size;
 
+    public MyMinHeap(int size) {
+        this.heap =  new MyArrayList<>();
+        this.size = size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T getMin(){
+        if (isEmpty()){
+            return null;
+        }
+       return heap.getFirst();
+    }
+
+    public T extractMin(){
+        if (isEmpty()){
+            return null;
+        }
+        T temp = heap.getFirst();
+        heap.set(0, heap.getLast());
+        heap.removeLast();
+        heapify(0);
+        return temp;
+    }
+
+    public void insert(T value){
+        heap.addLast(value);
+        traverseUP(size - 1);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void heapify(int index){
+        if(((Comparable<T>)heap.get(rightChild(index))).compareTo(heap.get(leftChild(index))) > 0){
+            if(((Comparable<T>)heap.get(index)).compareTo(heap.get(leftChild(index))) > 0){
+                swap(index, leftChild(index));
+                heapify(leftChild(index));
+            }
+        }
+        else {
+            if(((Comparable<T>)heap.get(index)).compareTo(heap.get(rightChild(index))) > 0){
+                swap(index, rightChild(index));
+                heapify(rightChild(index));
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void traverseUP(int index){
+        if(((Comparable<T>)heap.get(index)).compareTo(heap.get(parentOf(index))) < 0){
+            swap(index, parentOf(index));
+            traverseUP(parentOf(index));
+        }
+    }
+
+    private int leftChild(int index){
+        return index * 2 + 1;
+    }
+
+    private int rightChild(int index){
+        return index * 2 + 2;
+    }
+
+    private int parentOf(int index){
+        return (index - 1) / 2;
+    }
+
+    private void swap(int index1, int index2){
+        T temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
 }
